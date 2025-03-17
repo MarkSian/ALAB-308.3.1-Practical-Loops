f80b9967ca835121677cb6fa7884d0627ed51dd2
MarkSian <merksianipar@gmail.com>
2025-03-17 14:45:28 -0600

    Update index.js
    Part 2 Complete and setting the CSV constants for Part 3

--- index.js
+++ index.js
@@ -27,3 +27,32 @@ for (let i = 1; i <= 100; i++) {
 
 
 /* Part 2: Prime Time */
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+    for (let i = 3; i * i <= num; i += 2) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+let n = 11; // Example input
+
+let current = n + 1;
+
+while (true) {
+    if (isPrime(current)) {
+        console.log(`${current} is the next prime number after ${n}`);
+        break;
+    }
+    current++;
+}
+
+
+
+/* Part 3: Feeling loopy */
+const csvString1 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+const csvString2 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
