c98fab7dec9c3a050919e7ec9d8224ddd5053be5
MarkSian <merksianipar@gmail.com>
2025-03-17 12:39:47 -0600

    Create index.js
    Part 1
    Commented the deliverables

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Part 1: Fizz Buzz */
+// Accomplish The Following:
+// 1) Loop through all numbers from 1 to 
+// 2) If a number is divisible by 3, log "Fizz"
+// 3) If a number is divisible by 5, log "Buzz"
+// 4) If a number is divisible by both 3 and 5, log "FizzBuzz"
+// 5) If a number is not divisible by either 3 or 5, log the number
