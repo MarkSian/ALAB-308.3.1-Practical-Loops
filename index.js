be1c52347d70b57aa92eea498497e6c5ef3bf4fa
MarkSian <merksianipar@gmail.com>
2025-03-18 09:58:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -172,3 +172,10 @@ for (let j = 0; j < str.length; j++) {
     }
 
 }
+
+// Log
+// ID Name Occupation Age
+// 42 Bruce Knight 41
+// 57 Bob Fry Cook 19
+// 63 Blaine Quiz Master 58
+// 98 Bill Doctor’s Assistant 26
